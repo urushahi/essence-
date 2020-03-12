d7a3dfe0002ca97c5dde1686c8b39845c0d20d68
urushahi <mail.urushashahi@gmail.com>
2020-03-12 18:18:05 +0545

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+
+     $('.slickslider').slick({
+        dots:true,
+        slidesToShow:2,
+        slidesToScroll:1,
+        autoplay:true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                arrows: false,
+              
+              
+                slidesToShow: 1
+              }
+            },
+           
+          ]
+      })
